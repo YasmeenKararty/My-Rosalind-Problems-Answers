9ba967ced0a322671f04f2579692e4a06d89bae3
YasmeenKararty <66736704+YasmeenKararty@users.noreply.github.com>
2021-05-08 19:38:10 +0200

    Add files via upload
    This code is to count the occurrences of the nucleotide bases in a DNA sequence and display them to the user.

---
+++ Counting DNA nucleuotides.py
@@ -0,0 +1,9 @@
+DNAsequence="ATTCGTAATC" 
+countA=DNAsequence.count('A')
+countC=DNAsequence.count('C')
+countG=DNAsequence.count('G')
+countT=DNAsequence.count('T')
+print ('Base "A" occured',countA,'times.')
+print ('Base "C" occured',countC,'times.')
+print ('Base "G" occured',countG,'times.')
+print ('Base "T" occured',countT,'times.')
