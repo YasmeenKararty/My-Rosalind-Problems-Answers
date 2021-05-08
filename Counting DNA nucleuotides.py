88473b421171bfafd24dfddc6e3de1981be02bce
YasmeenKararty <66736704+YasmeenKararty@users.noreply.github.com>
2021-05-08 19:37:10 +0200

    Delete Counting DNA nucleuotides.py

--- Counting DNA nucleuotides.py
+++
@@ -1,6 +0,0 @@
-DNAsequence="ATTCGTAATC" 
-countA=DNAsequence.count('A')
-countC=DNAsequence.count('C')
-countG=DNAsequence.count('G')
-countT=DNAsequence.count('T')
-print (countA, countC, countG, countT )
